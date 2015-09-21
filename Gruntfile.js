4d10e640d332f039a9867774a3d4c4fb2a15e5fd
Dean <thainq@qsoftvietnam.com>
2015-09-21 23:49:59 +0700

    Building template structure

---
+++ Gruntfile.js
@@ -0,0 +1,86 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            src: {
+                files: ['**/*.scss', '**/*.php', '**/**/*.scss', ],
+                tasks: ['compass']
+            },
+            options: {
+                livereload: true
+            },
+            javascripts: {
+                files: "javascripts/app.js",
+                tasks: ['uglify']
+            }
+        },
+
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'assets',
+                    cssDir: '',
+                    imagesDir: 'images',
+                    javascriptsDir: 'javascripts',
+                    fontsDir: 'assets/fonts',
+                    outputStyle: 'compressed',
+                    relativeAssets: false,
+                    noLineComments: false,
+                    debugInfo: false
+                }
+            }
+        },
+
+        uglify: {
+            build: {
+                files: {                     
+                    'javascripts/app.min.js': ['javascripts/app.js']
+                }
+            },
+            compress: true
+        },
+
+         // run $ grunt copy from the terminal to copy the files listed below.
+         
+        copy: {
+            main: {
+
+                files: [
+                    {
+                        expand: true,
+                        dest: 'assets/stylesheets',
+                        src: ['**'],
+                        cwd: 'bower_components/bootstrap-sass/assets/stylesheets'
+                    }, {
+                        expand: true,
+                        dest: 'assets/fonts',
+                        src: ['**'],
+                        cwd: 'bower_components/bootstrap-sass/assets/fonts'
+                    }, {
+                        expand: true,
+                        dest: 'assets/',
+                        src: '_bootstrap.scss',
+                        cwd: 'bower_components/bootstrap-sass/assets/stylesheets'
+                    }, {
+                        expand: true,
+                        dest: 'javascripts',
+                        src: ['bootstrap.min.js'],
+                        cwd: 'bower_components/bootstrap-sass/assets/javascripts'
+                    }, {
+                        expand: true,
+                        dest: 'javascripts',
+                        src: ['bootstrap.js'],
+                        cwd: 'bower_components/bootstrap-sass/assets/javascripts'
+                    }
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');    
+    grunt.registerTask('default', ['copy','compass', 'uglify']);
+
+};
---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+add_action('wp_enqueue_scripts', 'fire_enqueue_assets');
+function fire_enqueue_assets() {
+    
+    // bower:css
+    // endbower
+
+    // bower:js
+    // endbower
+}
